2a41d575888a594e82a6fd55e2ef1ea25a8ffdca
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-07 12:43:52 +0300

    first commit

---
+++ solution/src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="header">Recipe App</div>
+      <div className="pages">
+        <
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
