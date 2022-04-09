752f31becc46b3ef63f560e1ab0e5d80625b3867
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-09 12:54:05 +0300

    finish

--- solution/src/components/navbar/NavbarStyles.jsx
+++ solution/src/components/navbar/NavbarStyles.jsx
@@ -1,32 +1,6 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 
-// export const Nav =styled.div`
-
-// display:flex;
-// justify-content:space-between;
-// align-items:center;
-// // border:1px solid red;
-// padding:1rem ;
-// background-color:bisque
-
-
-// `
-// export const Header=styled.div`
-// cursor:pointer;
-
-// `
-// export const Menu=styled.div`
-
-
-//  `
-
-// export const MenuLink =styled(Link)`
-// text-decoration:none;
-// margin:0.5rem
-
-// `
-
 
 // import { ExternalLink } from "react-external-link";
 export const Nav = styled.div`
--- solution/src/pages/home/Home.jsx
+++ solution/src/pages/home/Home.jsx
@@ -1,39 +1,120 @@
-import axios from "axios";
-import React, { useState } from "react";
+// import axios from "axios";
+// import React, { useState } from "react";
+// import Header from "../../components/header/Header";
+// import { ImgDiv, MainContainer,HomeImg} from "./HomeStyles"
+// import homeSvg from "../../assets/home.svg"
+// import RecipeCardComp from "./RecipeCardComp"
+// const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+// const API_ID = "527e8465";
+// const API_KEY = "aa32963a754d1bcda57f3e5648211fd8";
+
+// const Home = () => {
+//   const [food, setFood] = useState();
+//   const [query, setQuery] = useState("");
+//   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
+
+
+//   const url = `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&mealType=${meal}`;
+
+//   const getData = async () => {
+//     if (query) {
+//       const result = await axios.get(url);
+//       setFood(result.data.hits)
+//       // console.log(result);
+//       // console.log(result.data.hits);
+
+
+//     } 
+//     else {
+//       console.log("please fill the form");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Header
+//         setQuery={setQuery}
+//         getData={getData}
+//         setMeal={setMeal}
+//         mealTypes={mealTypes}
+//       />
+
+// {food?(
+//   <MainContainer>
+// {food.map((liste,index)=>(
+//   <RecipeCardComp key={index} recipe1={liste.recipe}/>
+// ))}
+//   </MainContainer>
+// ):<ImgDiv>
+// <HomeImg src={homeSvg}/></ImgDiv>}
+
+//     </div>
+//   );
+// };
+
+// export default Home;
+import axios from 'axios';
+import React, {useState} from 'react'
 import Header from "../../components/header/Header";
+import { ImgDiv, MainContainer,HomeImg } from './HomeStyles';
+import homeSvg from '../../assets/home.svg'
+import RecipeCardComp from "./RecipeCardComp"
 
 
-const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
-const API_ID = "527e8465";
-const API_KEY = "aa32963a754d1bcda57f3e5648211fd8";
+const APP_ID = "bfbb3efc"; 
+const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+/* buraya kendi id ve key imizi yaziyoruz*/
+
+
 
 const Home = () => {
-  const [food, setFood] = useState();
-  const [query, setQuery] = useState("");
-  const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
-
-
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&mealType=${meal}`;
-
+const [query, setQuery]=useState("");
+const[food, setFood]=useState();
+const mealTypes=["Breakfast", "Lunch","Dinner","Snack","Teatime"]
+const [meal,setMeal]=useState(mealTypes[0].toLowerCase());
+  
+const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+  
   const getData = async () => {
-    if (query) {
+    if(query){ 
       const result = await axios.get(url);
-      setFood(result.data.hits);
-    } else {
-      console.log("please fill the form");
-    }
-  };
+    setFood(result.data.hits)
+  //  console.log(result);
+    console.log(result.data.hits);
+  
+  }else {
+    console.log("please fill the form");
+  }
+      
 
+
+  }
+  
   return (
     <div>
-      <Header
-        setQuery={setQuery}
-        getData={getData}
-        setMeal={setMeal}
-        mealTypes={mealTypes}
-      />
-    </div>
-  );
-};
+<Header  setQuery={setQuery}
+getData={getData}
+mealTypes={mealTypes}
+setMeal={setMeal}
 
-export default Home;
+/>
+
+{food? (<MainContainer>
+{food.map((liste,index)=>(
+  <RecipeCardComp key={index} recipe1={liste.recipe}/>
+))}
+
+</MainContainer>
+
+):<ImgDiv>
+<HomeImg src={homeSvg}/>
+</ImgDiv>}
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
